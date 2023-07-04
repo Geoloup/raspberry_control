4b2f6f4291a67e7058d38a56979d36cdaf54372c
franck403 <79372051+franck403@users.noreply.github.com>
2023-07-04 15:56:54 -0400

    Update __init__.py

--- raspberrypi_control/__init__.py
+++ raspberrypi_control/__init__.py
@@ -1,1 +1,1 @@
-from raspberrypi_control import raspberry_command, timeout, raspberrypi, config, raspberry_command_add
+from .raspberrypi_control import raspberry_command, timeout, raspberrypi, config, raspberry_command_add
