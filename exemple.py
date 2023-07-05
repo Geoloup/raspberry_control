903ca67c0a3460352b0103652b724eea765518e8
franck403 <79372051+franck403@users.noreply.github.com>
2023-07-05 14:05:09 -0400

    Update exemple.py

--- exemple.py
+++ exemple.py
@@ -29,6 +29,7 @@ def other():
 
 
 if __name__ == "__main__":  # put all you're code to run at start here. Because if not the code will be run 2 time
+    rp.raspberrypi().set_raspberry_info("username here", "password here") # set login info here
     rp.raspberrypi().set_preparation("192.168.0.10", 8, 1)  # config locator for the raspberrypi
     rp.raspberrypi().local("192.168.0.10")  # set the start ip set in the line in the top
     rp.config("main")  # file name if this file (no .py)
